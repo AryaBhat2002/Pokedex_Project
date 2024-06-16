a3f7ec508caf28735b59611de5787de2861b33a0
Arya Sankar Bhattacharyya <aryasbhattacharyya@gmail.com>
2024-06-17 01:09:43 +0530

    Added Routing

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,20 +1,24 @@
+import { Link } from 'react-router-dom';
 import './Pokemon.css'
 import PropTypes from 'prop-types'
 
-function Pokemon({ name, image }) {
+function Pokemon({ name, image , id }) {
     return(
         <div className='pokemon'>
-            <div className='pokemon-name'>{name}</div>
-            <div>
-                <img className='pokemon-image' src={image} />
-            </div>
+            <Link to={`/pokemon/${id}`}>
+                <div className='pokemon-name'>{name}</div>
+                <div>
+                    <img className='pokemon-image' src={image} />
+                </div>
+            </Link>
         </div>
     )
 }
 
 Pokemon.propTypes = {
     name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
+    image: PropTypes.string.isRequired,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired
 };
 
 export default Pokemon;
